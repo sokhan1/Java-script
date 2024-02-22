00690d50f2d19db03796aec5530562c74161cb97
SEOKHWAN KIM <99132328+sokhan1@users.noreply.github.com>
2024-02-22 13:20:09 +0900

    Create BLINKING LETTER FUNCTION on the home page.js

---
+++ BLINKING LETTER FUNCTION on the home page.js
@@ -0,0 +1,40 @@
+let object = document.querySelector("#name");
+
+function base(){
+    let string=["Dental Clinical Square","SUPPORTED BY KOREC"
+    ,"SEOKHWAN KIM","Dental Clinical Square"];
+    let StringSelected= string[Math.floor(Math.random() * string.length)];
+    let selectStringArr = StringSelected.split("");
+    
+    return selectStringArr;
+}
+
+
+// print out one letter
+function motion(letter){
+
+    console.log(letter); //check in console of browser
+    if(letter.length>0){
+        object.textContent +=letter.shift();
+        setTimeout(function(){
+            motion(letter);
+
+        },80)
+    }
+    else{
+        setTimeout(clear, 3000);
+    }
+}
+
+function clear(){
+    object.textContent=" ";
+    motion(base());
+}
+
+motion(base());
+
+//blink effect
+function blink(){
+    object.classList.toggle("active");
+}
+setInterval(blink,400);
